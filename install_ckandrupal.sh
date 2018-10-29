81062726b7c6c937b53125a7bb48c86e822bcac2
Adama DIENG <diengadama1@gmail.com>
2018-10-29 16:10:53 +0100

    Ajout de sudo apt install python-pastescript

--- install_ckandrupal.sh
+++ install_ckandrupal.sh
@@ -5,8 +5,8 @@
 #
 #
 #
-
-sudo apt-get install build-essential libxml2-dev libxslt-dev wget postgresql libpq-dev git-core python-dev python-psycopg2 python-virtualenv subversion mercurial
+sudo apt install -y python-pastescript
+sudo apt-get install -y build-essential libxml2-dev libxslt-dev wget postgresql libpq-dev git-core python-dev python-psycopg2 python-virtualenv subversion mercurial
 virtualenv ckan-drupal
 . ckan-drupal/bin/activate
 pip install -r ckan-drupal/ckan/requires/lucid_missing.txt -r ckan-drupal/ckan/requires/lucid_conflict.txt -r ckan-drupal/ckan/requires/lucid_present.txt
